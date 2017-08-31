143072f1b9118eef589e41bb5c066ac3eaa6c945
Andrija <andrija.kuzmanov@gmail.com>
2017-08-31 20:03:58 +0200

    Adding a projects page

--- server.js
+++ server.js
@@ -49,6 +49,14 @@ app.get('/',(req, res) => {
 
 });
 
+app.get('/projects', (req, res) => {
+
+  res.render('projects.hbs', {
+    pageTitle: 'Portfolio page'
+    welcomeMessage: 'Welcome to projects'
+  })
+
+});
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
